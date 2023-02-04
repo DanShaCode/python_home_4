d6ab2603e1f4706968a3794926e6b70e90258526
Danis Sharipov <danshadance@mail.ru>
2023-02-04 22:28:13 +0600

    task2 done

---
+++ task2.py
@@ -0,0 +1,30 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+print()
+n = int(input("Введите число: "))
+print()
+
+user_list = []
+
+result = n
+divider = 2
+
+while result > 1:
+    if result % divider == 0:
+        result = int(result/divider)
+        user_list.append(divider)
+        divider = 2
+    else:
+        divider += 1
+
+count = 0
+len_list = len(user_list)
+
+print("Число", n, "разложенное на простые множители: ", end = "")
+for i in user_list:
+    if count == len_list - 1:
+        print(user_list[count])
+        break
+    else:
+        print(user_list[count], end = "*")
+        count += 1
