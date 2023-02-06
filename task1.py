8cc1b796eec16852bc734c60fca347cfa7a033ee
Danis Sharipov <danshadance@mail.ru>
2023-02-06 23:20:48 +0600

    task1 bug fixes

--- task1.py
+++ task1.py
@@ -6,7 +6,7 @@ print()
 d = input("Задайте число d сюда ---> ")
 print()
 
-if float(d) > (10**-1) and float(d) < (10**-10) or float(d) == (10**-1) or float(d) == (10**-10):
+if float(d) < (10**-1) and float(d) > (10**-10) or float(d) == (10**-1) or float(d) == (10**-10):
     count_numbers_after = - 1
 
     for i in d:
@@ -16,7 +16,7 @@ if float(d) > (10**-1) and float(d) < (10**-10) or float(d) == (10**-1) or float
     if count_numbers_after == 0:
         print(3)
     else:
-        iterations = 1000000
+        iterations = 100000
         count = 0
         denominator_num = 3
         result = 4
@@ -36,7 +36,7 @@ if float(d) > (10**-1) and float(d) < (10**-10) or float(d) == (10**-1) or float
         count = 0
         numbers_after = 2 + count_numbers_after
 
-        print("Число Пи с точностью", count_numbers_after, "знак(а) после запятой: ", end = "")
+        print("Число Пи с точностью", count_numbers_after, "знака(ов) после запятой: ", end = "")
         for i in result:
             print(i, end = "")
             count += 1
